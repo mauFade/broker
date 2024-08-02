4e2d3ec8dd989ca56903f6731086aa04eabdfcc5
Mauricio Cardoso <mauricio.cds00@gmail.com>
2024-08-02 17:46:49 -0300

    feat: routes

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/mauFade/broker/database"
+)
 
+func main() {
+	database.ConnectDB()
+
+	app := fiber.New()
+
+	setupRoutes(app)
+
+	app.Listen(":8081")
 }
---
+++ cmd/routes.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/")
+}
